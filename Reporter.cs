39f654a20caa2c61dd4db480190e23e78c6ae221
joe10111 <centeno.joe@gmail.com>
2023-06-07 12:29:38 -0700

    Added Reporter Class

---
+++ Reporter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newspaper
+{
+    public class Reporter
+    {
+        public string Name;
+        public string Speciality;
+        public string Articles; // Place holder string untill article class is made
+
+    }
+}
